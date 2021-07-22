70c45a2d1b7be3ee35cd3bfb6b4726462d1ca2d3
skjaved633 <skjaved633@gmail.com>
2021-07-22 11:04:20 +0530

    updates

--- myapp/src/components/Footer.jsx
+++ myapp/src/components/Footer.jsx
@@ -5,18 +5,44 @@ const Footer=()=>{
         <div>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <div>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos doloremque quo sapiente quos autem placeat et aliquam eveniet blanditiis, accusamus corrupti animi reprehenderit unde vel dolorum in. Commodi, asperiores exercitationem?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime odio asperiores? In eius deserunt corrupti maxime vel, quas aliquid cumque cupiditate quod, laudantium quae libero esse! Impedit quam rem fuga praesentium delectus, animi nemo vero autem sit laborum nam voluptates, doloribus mollitia voluptatum saepe labore voluptas laudantium. Eos nam nihil blanditiis sint a in quas! Commodi id autem dolor in eum minus magnam voluptatem, inventore minima delectus harum voluptate accusantium. Expedita maxime, consequuntur, velit exercitationem corrupti libero sint beatae accusamus cumque illo atque obcaecati necessitatibus repellat quas reprehenderit similique amet nulla tempora minus neque! Expedita repellat aliquam recusandae aspernatur, ea commodi. Nulla totam laboriosam nihil dolorum nisi. Veniam voluptatibus provident sapiente nobis sit nesciunt animi a doloremque cupiditate, molestiae soluta quod est aspernatur beatae similique consectetur voluptatem assumenda. Soluta recusandae non libero similique voluptatibus ipsam, tenetur cum qui quis, rerum aut possimus ullam aliquid, pariatur inventore ipsa. Obcaecati a explicabo aliquid? Dignissimos culpa rem dolore beatae dicta officiis dolorem blanditiis ad provident voluptate perferendis, animi saepe natus molestiae, aspernatur nisi amet, delectus necessitatibus temporibus? Impedit, non incidunt quasi nihil atque accusamus praesentium. Numquam libero magni laborum dolorum consequatur velit ea aut saepe, quas eveniet officia, atque eligendi temporibus, ipsa sed odio alias totam. Quasi dolorem impedit quaerat beatae eius fugiat eum quas similique sapiente doloribus eos eligendi aliquam, asperiores nostrum, voluptates esse sed, a molestias labore assumenda. Nihil magnam beatae optio inventore laboriosam excepturi animi incidunt esse, natus aspernatur, voluptate quod dolores perferendis fuga, maiores autem? Laudantium dolorem officiis non vel sapiente ipsam iusto est commodi porro velit nesciunt fugiat impedit, consequatur placeat necessitatibus assumenda, nihil numquam amet reiciendis veniam vero facere rem nam. Dolor natus illo quo dolorem distinctio atque praesentium! Dolorum, nam corrupti fugit illo saepe voluptates dolor! Iure delectus magnam distinctio, pariatur voluptates aperiam sint maiores.
         </div>
         <div id="footer">
             <footer>
-                <div>
-                    <h1>Belivers</h1> <br /> <hr /> <br />
-                    <a href="instagram.com"><i class="fab fa-instagram"></i></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
+                <div className="footer" id="social">
+                    <h3>WeGiver</h3> <br /> <hr /> <br />
+                    <div className="items" id="icons">
+                        <a href="instagram.com"><i class="fab fa-instagram"></i></a>
+                        <a href="facebook.com"><i class="fab fa-facebook-square"></i></a>
+                        <a href="twitter.com"><i class="fab fa-twitter-square"></i></a>
+                        <a href="linkedin.com"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="youtube.com"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+                <div className="footer" id="about">
+                    <h3>About us</h3> <br /> <hr /><br />
+                    <div className="items">
+                    <a href="">The Foundation</a><br />
+                    <a href="">Mission Partners</a><br />
+                    <a href="">Events Calendar</a><br />
+                    <a href="">Blog and News</a><br />
+                    </div>
+                </div>
+                <div className="footer" id="fundraising">
+                    <h3>Fundraising</h3> <br /><hr /><br />
+                    <div className="items">
+                    <a href="">How to Raise Fund</a><br />
+                    <a href="">Perks of fundraisers</a><br />
+                    </div>
+                </div>
+                <div className="footer" id="policy">
+                    <h3>Policies</h3><br /><hr /><br />
+                    <div className="items">
+                        <a href="">Terms and Conditions</a><br />
+                        <a href="">Privacy Policy</a><br />
+                        <a href="">Notice of Privacy Practices</a><br />
+                        <a href="">Notice of Nondiscrimination</a><br />
+                    </div>
                 </div>
             </footer>
         </div>
