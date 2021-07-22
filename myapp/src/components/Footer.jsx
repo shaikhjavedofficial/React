85edfe1ef9c5e5065c67abcd720e36bd532c4a2c
skjaved633 <skjaved633@gmail.com>
2021-07-22 13:05:39 +0530

    commit

--- myapp/src/components/Footer.jsx
+++ myapp/src/components/Footer.jsx
@@ -5,12 +5,13 @@ const Footer=()=>{
         <div>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <div>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus maxime odio asperiores? In eius deserunt corrupti maxime vel, quas aliquid cumque cupiditate quod, laudantium quae libero esse! Impedit quam rem fuga praesentium delectus, animi nemo vero autem sit laborum nam voluptates, doloribus mollitia voluptatum saepe labore voluptas laudantium. Eos nam nihil blanditiis sint a in quas! Commodi id autem dolor in eum minus magnam voluptatem, inventore minima delectus harum voluptate accusantium. Expedita maxime, consequuntur, velit exercitationem corrupti libero sint beatae accusamus cumque illo atque obcaecati necessitatibus repellat quas reprehenderit similique amet nulla tempora minus neque! Expedita repellat aliquam recusandae aspernatur, ea commodi. Nulla totam laboriosam nihil dolorum nisi. Veniam voluptatibus provident sapiente nobis sit nesciunt animi a doloremque cupiditate, molestiae soluta quod est aspernatur beatae similique consectetur voluptatem assumenda. Soluta recusandae non libero similique voluptatibus ipsam, tenetur cum qui quis, rerum aut possimus ullam aliquid, pariatur inventore ipsa. Obcaecati a explicabo aliquid? Dignissimos culpa rem dolore beatae dicta officiis dolorem blanditiis ad provident voluptate perferendis, animi saepe natus molestiae, aspernatur nisi amet, delectus necessitatibus temporibus? Impedit, non incidunt quasi nihil atque accusamus praesentium. Numquam libero magni laborum dolorum consequatur velit ea aut saepe, quas eveniet officia, atque eligendi temporibus, ipsa sed odio alias totam. Quasi dolorem impedit quaerat beatae eius fugiat eum quas similique sapiente doloribus eos eligendi aliquam, asperiores nostrum, voluptates esse sed, a molestias labore assumenda. Nihil magnam beatae optio inventore laboriosam excepturi animi incidunt esse, natus aspernatur, voluptate quod dolores perferendis fuga, maiores autem? Laudantium dolorem officiis non vel sapiente ipsam iusto est commodi porro velit nesciunt fugiat impedit, consequatur placeat necessitatibus assumenda, nihil numquam amet reiciendis veniam vero facere rem nam. Dolor natus illo quo dolorem distinctio atque praesentium! Dolorum, nam corrupti fugit illo saepe voluptates dolor! Iure delectus magnam distinctio, pariatur voluptates aperiam sint maiores.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt rerum quasi quam voluptatibus officia dignissimos sapiente eum, illum porro ullam, similique itaque voluptates nostrum quaerat maiores omnis. Amet maxime dolores voluptatem assumenda excepturi eius eveniet error sit quisquam ut maiores officiis enim architecto sint tenetur, iste inventore earum laboriosam obcaecati qui? Soluta dolorum consequuntur alias vel dolores, placeat expedita cum, nostrum laboriosam voluptate nemo quidem quas atque quos porro enim delectus repellendus maxime accusamus voluptates exercitationem. Deleniti repellat culpa officiis dolorem sed ad soluta error distinctio unde! Libero esse nostrum fuga aliquam ullam inventore laborum similique eius eum fugit odio iste corporis labore officia odit soluta dolorem ea asperiores et delectus, quis illo quisquam. Repudiandae necessitatibus earum omnis placeat, accusamus cum delectus commodi quod tenetur non debitis quasi ratione doloremque nobis vero impedit quisquam expedita explicabo deleniti vitae. Dignissimos numquam architecto nihil nostrum illo sapiente quo accusamus eveniet minus sint, veniam, alias at facilis consequuntur natus quisquam porro culpa, quas placeat? Debitis asperiores, aliquam veritatis aut enim officiis repellendus nemo facere deleniti expedita obcaecati doloremque inventore quaerat exercitationem, qui hic, soluta libero delectus consectetur quos ad cumque vero. Fuga impedit ipsam velit veniam cumque, illum adipisci fugiat ullam aspernatur inventore error amet consequatur sapiente, nemo quaerat corporis facilis excepturi architecto magnam. Quasi recusandae odit enim, excepturi exercitationem ipsa dolorum quas reiciendis, voluptatum neque repellat. Atque molestias deleniti ex totam iste distinctio aperiam natus corrupti sed libero ab dolorem fugit id enim, esse alias temporibus ipsa porro in quos quas perferendis minima quam! Neque placeat officiis at, quidem voluptates et nam a distinctio molestiae, reiciendis labore itaque voluptatibus quos! Ex error sapiente doloremque. Quod animi totam molestias repellendus exercitationem officia sequi sed quae, atque blanditiis! Omnis debitis laborum tenetur itaque exercitationem facere delectus, nostrum, voluptates provident reprehenderit soluta consectetur architecto nisi quidem animi at perferendis ducimus eius id earum distinctio eum? Eius fugiat numquam impedit, explicabo cum perspiciatis? Eveniet ducimus impedit sed quibusdam quae dicta cumque voluptatum neque illo? Maxime, ratione! Quisquam excepturi tempora quas nam earum beatae blanditiis numquam magni sed maxime? Velit quia nam aliquam nostrum ut doloremque labore, ullam laboriosam ipsam, voluptatum pariatur sapiente id asperiores facere repudiandae natus neque earum. Nobis, itaque. Maiores culpa, ad consectetur nemo dolor totam quos magni itaque distinctio quasi aut maxime voluptates similique. Nesciunt nam fugiat atque natus earum labore minima recusandae nemo explicabo enim ipsa, quod deleniti facilis animi temporibus nulla reiciendis, itaque incidunt eum, aliquam nobis optio necessitatibus vel laborum. Accusantium atque asperiores laudantium velit alias unde similique at modi sequi quo recusandae, minima maxime iste ad? Vel doloribus dolore, ipsum aperiam error quo numquam laborum corrupti eligendi? Ad alias doloribus quam repellat ratione consequuntur unde, id officiis maxime fugiat deleniti delectus et velit sint tempore libero quaerat veritatis quia molestias vero corrupti fuga esse. Fugiat possimus, pariatur, officiis repellat sint odio dignissimos corporis repudiandae velit minus quia fuga. Maiores repellat, ipsum voluptatem fuga vero at, minus cum mollitia modi commodi consequatur perspiciatis. Dicta voluptate nihil nesciunt iusto? Iure, dolorum?
         </div>
+        <br /><br />
         <div id="footer">
             <footer>
                 <div className="footer" id="social">
-                    <h3>WeGiver</h3> <br /> <hr /> <br />
+                    <h2>WeGiver</h2> <br /> <hr /> <br />
                     <div className="items" id="icons">
                         <a href="instagram.com"><i class="fab fa-instagram"></i></a>
                         <a href="facebook.com"><i class="fab fa-facebook-square"></i></a>
@@ -18,9 +19,17 @@ const Footer=()=>{
                         <a href="linkedin.com"><i class="fab fa-linkedin-in"></i></a>
                         <a href="youtube.com"><i class="fab fa-youtube"></i></a>
                     </div>
+                    <br />
+                    <hr />
+                    <br />
+                    <div id="contact">
+                        <h3>Contact us</h3><hr /><br />
+                        Email:<a href="skjaved633@gmail.com">skjaved633@gmail.com</a><br />
+                        Phone:+918275199525
+                    </div>
                 </div>
                 <div className="footer" id="about">
-                    <h3>About us</h3> <br /> <hr /><br />
+                    <h3>About us</h3> <br /><hr /><br />
                     <div className="items">
                     <a href="">The Foundation</a><br />
                     <a href="">Mission Partners</a><br />
@@ -44,6 +53,12 @@ const Footer=()=>{
                         <a href="">Notice of Nondiscrimination</a><br />
                     </div>
                 </div>
+            </footer>
+        </div>
+        <hr />
+        <div>
+            <footer id="footer2">
+                <p>copyright &copy;2021 PickupBiz | Developed & Designed By PickupBiz </p>
             </footer>
         </div>
         </div>
