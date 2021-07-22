d24a2254bab1476beba0cf94e61a0c6919a0a5e7
skjaved633 <skjaved633@gmail.com>
2021-07-22 09:12:34 +0530

    commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,44 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
-import { CourseInfo } from "./components/Courseinfo"
+import "./components/Game"
 
 function App() {
   return (
-    <div className="App">
-      <h1>List Of Institute</h1>
-  <table>
-  <tr>
-  <td><CourseInfo courseName="HTML/CSS" institute="PickupBiz" city="Nanded" contact="8766498998"/></td>
-  <td><CourseInfo courseName="JS" institute="PickupBiz" city="pune" contact="8275199525"/></td>
-  <td><CourseInfo courseName="React" institute="PickupBiz" city="Hyd" contact="9028111155"/></td>
-  </tr>
-  <tr>
-    <td><hr /></td>
-    <td><hr /></td>
-    <td><hr /></td>
-  </tr>
-  <tr>
-  <td><CourseInfo courseName="HTML/CSS" institute="PickupBiz" city="Nanded" contact="8766498998"/></td>
-  <td><CourseInfo courseName="JS" institute="PickupBiz" city="pune" contact="8275199525"/></td>
-  <td><CourseInfo courseName="React" institute="PickupBiz" city="Hyd" contact="9028111155"/></td>
-  </tr>
-  <tr>
-    <td><hr /></td>
-    <td><hr /></td>
-    <td><hr /></td>
-  </tr>
-  <tr>
-  <td><CourseInfo courseName="HTML/CSS" institute="PickupBiz" city="Nanded" contact="8766498998"/></td>
-  <td><CourseInfo courseName="JS" institute="PickupBiz" city="pune" contact="8275199525"/></td>
-  <td><CourseInfo courseName="React" institute="PickupBiz" city="Hyd" contact="9028111155"/></td>
-  </tr>
-  <tr>
-    <td><hr /></td>
-    <td><hr /></td>
-    <td><hr /></td>
-  </tr>
-  </table>
+  //   <div className="App">
+  //     <h1>List Of Institute</h1>
+  // <table>
+  // <tr>
+  // <td><CourseInfo courseName="HTML/CSS" institute="PickupBiz" city="Nanded" contact="8766498998"/></td>
+  // <td><CourseInfo courseName="JS" institute="PickupBiz" city="pune" contact="8275199525"/></td>
+  // <td><CourseInfo courseName="React" institute="PickupBiz" city="Hyd" contact="9028111155"/></td>
+  // </tr>
+  // <tr>
+  //   <td><hr /></td>
+  //   <td><hr /></td>
+  //   <td><hr /></td>
+  // </tr>
+  // <tr>
+  // <td><CourseInfo courseName="HTML/CSS" institute="PickupBiz" city="Nanded" contact="8766498998"/></td>
+  // <td><CourseInfo courseName="JS" institute="PickupBiz" city="pune" contact="8275199525"/></td>
+  // <td><CourseInfo courseName="React" institute="PickupBiz" city="Hyd" contact="9028111155"/></td>
+  // </tr>
+  // <tr>
+  //   <td><hr /></td>
+  //   <td><hr /></td>
+  //   <td><hr /></td>
+  // </tr>
+  // <tr>
+  // <td><CourseInfo courseName="HTML/CSS" institute="PickupBiz" city="Nanded" contact="8766498998"/></td>
+  // <td><CourseInfo courseName="JS" institute="PickupBiz" city="pune" contact="8275199525"/></td>
+  // <td><CourseInfo courseName="React" institute="PickupBiz" city="Hyd" contact="9028111155"/></td>
+  // </tr>
+  // <tr>
+  //   <td><hr /></td>
+  //   <td><hr /></td>
+  //   <td><hr /></td>
+  // </tr>
+  // </table>
+  // </div>
+  <div>
+    <table>
+      <tr>
+        <td id="11">x</td>
+        <td id="12">x</td>
+        <td id="13">x</td>
+      </tr>
+      <tr>
+        <td id="21">x</td>
+        <td id="22">x</td>
+        <td id="23">x</td>
+      </tr>
+      <tr>
+        <td id="31">x</td>
+        <td id="32">x</td>
+        <td id="33">x</td>
+      </tr>
+    </table>
   </div>
+
     ); 
 }
 
---
+++ myapp/src/components/Game.jsx
@@ -0,0 +1,9 @@
+import react from 'React';
+import "./Game.css"
+const game=()=>{
+    var a;
+    return(
+        a
+    );
+}
+export default game;
