448bd9dbf4a465bcee271b272b849d595fd031db
skjaved633 <skjaved633@gmail.com>
2021-07-26 16:04:57 +0530

    commit

--- pract3/src/Component/layout/GridLayout.jsx
+++ pract3/src/Component/layout/GridLayout.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-export const GridLayout=()=>{
-    return(
-        
-    );
-}
+export const GridLayout = () => {
+  return (
+    <h1>
+      <p>
+        <p></p>
+      </p>
+    </h1>
+  );
+};
