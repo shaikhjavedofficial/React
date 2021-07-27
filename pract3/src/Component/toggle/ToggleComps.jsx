d9b0cb0ac2dd6bf9f89887d51705cd96a839068c
skjaved633 <skjaved633@gmail.com>
2021-07-27 16:12:27 +0530

    update

--- pract3/src/Component/toggle/ToggleComps.jsx
+++ pract3/src/Component/toggle/ToggleComps.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Image, Row, Col, Button } from "react-bootstrap";
 export const ToggleComps = () => {
   return (
