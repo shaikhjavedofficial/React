b8a456f73ebc5364ce6483b3a2a0b517fdd36ab8
skjaved633 <skjaved633@gmail.com>
2021-07-31 20:29:57 +0530

    commit

--- pract3/src/Component/search/EmpParent.jsx
+++ pract3/src/Component/search/EmpParent.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Form, Navbar } from "react-bootstrap";
 import { Emp } from "./Emp";
 import { EmpData } from "./EmpData";
 
 export const EmpParent = () => {
   return (
     <div>
+      <Navbar>
+        <h1>Employee Details</h1>
+        <Form.Control type="search" />
+      </Navbar>
       {EmpData.Data.map((elem) => (
         <Emp
           Name={elem.Name}
