cc94b62619d2fee499b586986bace0bf525b4e3e
skjaved633 <skjaved633@gmail.com>
2021-07-31 21:32:07 +0530

    commit

--- pract3/src/Component/search/Emp.jsx
+++ pract3/src/Component/search/Emp.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Emp.css";
-export const Emp = ({ Name, IMG, Dept, Address }) => {
+export const Emp = ({ FirstName, LastName, IMG, Dept, Address }) => {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={IMG} />
       <Card.Body>
-        <Card.Title>Name : {Name}</Card.Title>
+        <Card.Title>
+          Name : {FirstName} {LastName}
+        </Card.Title>
         <Card.Text>
           Address : {Address} <br />
           Department : {Dept}
--- pract3/src/Component/search/EmpParent.jsx
+++ pract3/src/Component/search/EmpParent.jsx
@@ -1,18 +1,39 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Form, Navbar } from "react-bootstrap";
 import { Emp } from "./Emp";
 import { EmpData } from "./EmpData";
 
 export const EmpParent = () => {
+  const [EmpArray, setEmpArray] = useState(EmpData.Data);
+  const [inputText, setInputText] = useState("");
+  useEffect(() => {
+    const filterArray = EmpArray.filter(
+      (item) =>
+        item.FirstName.toLowerCase().includes(inputText.toLowerCase()) ||
+        item.LastName.toLowerCase().includes(inputText.toLowerCase()) ||
+        item.Dept.toLowerCase().includes(inputText.toLowerCase()) ||
+        item.Address.toLowerCase().includes(inputText.toLowerCase())
+    );
+    if (inputText === "") setEmpArray(EmpData.Data);
+    else setEmpArray(filterArray);
+  }, [inputText]);
+  console.log(EmpArray);
   return (
-    <div>
+    <div className="ebody">
       <Navbar>
         <h1>Employee Details</h1>
-        <Form.Control type="search" />
+        <Form.Control
+          type="search"
+          onChange={(e) => setInputText(e.target.value)}
+        />
       </Navbar>
-      {EmpData.Data.map((elem) => (
+
+      {EmpArray.map((elem) => (
         <Emp
-          Name={elem.Name}
+          FirstName={elem.FirstName}
+          LastName={elem.LastName}
           IMG={elem.IMG}
           Address={elem.Address}
           Dept={elem.Dept}
