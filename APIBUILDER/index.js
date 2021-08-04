06d93fc5bd2bd9cb566e0518a5c41e749503af05
skjaved633 <skjaved633@gmail.com>
2021-08-04 16:50:11 +0530

    commit

--- APIBUILDER/index.js
+++ APIBUILDER/index.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const app = express()
 app.get("/", (req, res) => {
-    res.send("i am from Pickupbiz......");
+    res.send("I am from Pickupbiz......");
+})
+app.get("/emp", (req, res) => {
+    res.send("Hire Employee")
+})
+app.get("/dept", (req, res) => {
+    res.send("This is the Department")
 })
 if (app.listen(40)) {
     console.log("connected")
