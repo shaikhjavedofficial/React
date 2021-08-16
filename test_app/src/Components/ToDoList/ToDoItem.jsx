a31cfe8b85c2d43c8f2a0d6fd79c32c4c1509904
skjaved633 <skjaved633@gmail.com>
2021-08-16 17:31:20 +0530

    commit

--- test_app/src/Components/ToDoList/ToDoItem.jsx
+++ test_app/src/Components/ToDoList/ToDoItem.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Row, Col, Form } from "react-bootstrap";
 export const ToDoItem = ({ item }) => {
+  const [checkBox, setCheckBox] = useState(false);
   return (
     <React.Fragment>
       <Card className="todoitem">
         <Row>
           <Col md={2}>
-            <Form.Check></Form.Check>
+            <Form.Check onClick={() => setCheckBox(!checkBox)}></Form.Check>
             {/* checked={true} */}
           </Col>
           <Col md={10}>
