5e0ea0fa4bcca262c0e35b44e9e7df40fa414943
skjaved633 <skjaved633@gmail.com>
2022-05-29 06:07:22 +0530

    .

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { Formik } from "formik";
 
 function App() {
   const [formData, setformData] = useState({
@@ -9,32 +10,63 @@ function App() {
     pass: "",
   });
   const { name, pass, email } = formData;
+  const [saveData, setSaveData] = useState([]);
+  const onInput = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    setformData(...formData, [e.target.name, e.target.value]);
+    setSaveData([...saveData, formData]);
+    console.log(saveData);
   };
   return (
     <div style={{ "text-align": "center" }}>
-      <form action="">
+      <form action="" onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="">Name : </label>
         <br />
-        <input type="text" name="name" id="name" value={name} />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => onInput(e)}
+        />
         <br />
         <label htmlFor="">Email : </label>
         <br />
-        <input type="text" name="email" id="email" value={email} />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => onInput(e)}
+        />
         <br />
         <label htmlFor="">Password : </label>
         <br />
-        <input type="text" name="pass" id="pass" value={pass} />
+        <input
+          type="text"
+          name="pass"
+          id="pass"
+          value={pass}
+          onChange={(e) => onInput(e)}
+        />
         <br />
-        <button onClick={() => handleSubmit()}>submit</button>
+        <button type="submit">submit</button>
       </form>
       <label>form Data</label>
-      <h1>Name : {name}</h1>
-      <h1>Email : {email}</h1>
-      <h1>Password : {pass}</h1>
+      {saveData.map((item) => {
+        return (
+          <div>
+            <h1>Name : {item.name}</h1>
+            <h1>Email : {item.email}</h1>
+            <h1>Password : {item.pass}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
