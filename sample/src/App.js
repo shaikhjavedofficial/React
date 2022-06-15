2455a7548a3681a5864ce19e0573a6b2a88e77ff
shaikhjavedofficial <skjaved633@gmail.com>
2022-06-15 14:11:35 +0530

    new

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap/Button';
-import {CourseLand, Courses} from "../src/Components/Reducer/CourseLand";
-import {CardParent} from "./Components/Cards/CardParent"
-import {Counter} from "./Components/Counter";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap/Button";
+import { CourseLand, Courses } from "../src/Components/Reducer/CourseLand";
+import { CardParent } from "./Components/Cards/CardParent";
+import { Counter } from "./Components/Counter";
+import { PFooter } from "./Components/Z_Footer/PFooter";
 
 function App() {
   return (
     <div className="App">
-      <CourseLand/>
-      <CardParent/>
-      <Counter/>
+      <CourseLand />
+      <CardParent />
+      <Counter />
+      <br />
+      <br />
+      <PFooter />
     </div>
   );
 }
---
+++ sample/src/Components/Z_Footer/PFooter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FooterMain } from "./Footer";
+
+export const PFooter = () => {
+  return (
+    <div>
+      <FooterMain />
+    </div>
+  );
+};
