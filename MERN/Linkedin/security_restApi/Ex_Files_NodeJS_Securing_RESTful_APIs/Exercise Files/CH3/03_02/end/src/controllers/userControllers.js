6c34ebc5108a6db11b6f525ff4cd3a2afb09b6d4
shaikhjavedofficial <skjaved633@gmail.com>
2022-06-29 16:38:10 +0530

    new

---
+++ MERN/Linkedin/security_restApi/Ex_Files_NodeJS_Securing_RESTful_APIs/Exercise Files/CH3/03_02/end/src/controllers/userControllers.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { UserSchema } from '../models/userModel';
+
+const User = mongoose.model('User', UserSchema);
+
+export const loginRequired = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        return res.status(401).json({ message: 'Unauthorized user!'});
+    }
+}
+
+export const register = (req, res) => {
+    const newUser = new User(req.body);
+    newUser.hashPassowrd = bcrypt.hashSync(req.body.password, 10);
+    newUser.save((err, user) => {
+        if (err) {
+            return res.status(400).send({
+                message: err
+            });
+        } else {
+            user.hashPassword = undefined;
+            return res.json(user); 
+        }
+    })
+}
