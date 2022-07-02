e71e735bc2bf36db7e04d69946a8d03807e98552
shaikhjavedofficial <skjaved633@gmail.com>
2022-07-02 10:31:03 +0530

    new

---
+++ MERN/Linkedin/security_restApi/secureBackEndApp/ReadMe.md
@@ -0,0 +1,6 @@
+this is an secure app
+
+1. install 'npm i bcrypt jsonwebtoken'
+2. to secure api's many different companies are offering tools,
+   azure, aws, firebase, mongodb, and third party libraries like, sails, loopback, strapi, Auth0(easy)
+   .
--- MERN/Linkedin/security_restApi/secureBackEndApp/index.js
+++ MERN/Linkedin/security_restApi/secureBackEndApp/index.js
@@ -1,18 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import { Routes } from "./src/Routes/routes";
+import jsonwebtoken from "jsonwebtoken";
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  "mongodb+srv://skjaved633:SKsun@10@cluster0.1ntth.mongodb.net/?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.static("public"));
 app.use(
@@ -21,6 +20,33 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+// jwt setup
+app.use((req, res, next) => {
+  // console.log(req.headers);
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "JWT"
+  ) {
+    console.log("auth is running");
+    jsonwebtoken.verify(
+      req.headers.authorization.split(" ")[1],
+      "RESTAPIs",
+      (err, decode) => {
+        if (err) req.user = undefined;
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`the app is running on ${PORT}`);
 });
+
+Routes(app);
